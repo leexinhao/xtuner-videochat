8734c4b134dfab474c17841657fa7dc85dc57a38
曾祥宇 <zengxiangyu@pjlab.org.cn>
2025-12-23 20:37:31 +0800

    删除无用文件

--- see_jsonl.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("/mnt/petrelfs/zengxiangyu/Research_lixinhao/videochat3_data_annoations/image/DenseFusion_xtuner_qwen3_recap_clean_repeat_2round.jsonl", "r") as fr:
-    for line in fr:
-        print(json.loads(line))
-        break
--- test.py
+++
@@ -1,4 +0,0 @@
-# from datasets import Dataset, concatenate_datasets # 注释掉后不会报错
-import pyarrow
-# import datasets
-from petrel_client.client import Client
